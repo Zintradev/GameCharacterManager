bab80e436def053b52da7d8f4f45a5e46102160b
Zintra <136631919+Zintradev@users.noreply.github.com>
2025-06-20 00:31:39 +0200

    Update and rename main.c to CharacterManagerFile.c

--- CharacterManagerFile.c
+++ CharacterManagerFile.c
@@ -26,7 +26,7 @@ typedef struct{
 void llenarConjuntoPersonajes(conjuntoPersonajes * juegosLocales) //Paso por referencia pq vamos a modificar
 {
     int cantidadNuevaJuegos;
-    printf("\n\t¿Cuantos personajes nuevos quieres agregar? -> ");
+    printf("\n\tÂ¿Cuantos personajes nuevos quieres agregar? -> ");
     scanf("%d", &cantidadNuevaJuegos);
 
     // Reasignar memoria para contener los personajes anteriores y los nuevos
@@ -34,10 +34,10 @@ void llenarConjuntoPersonajes(conjuntoPersonajes * juegosLocales) //Paso por ref
 
     // Llenado de los nuevos personajes
     for (int i = juegosLocales->numeroJuegos; i < juegosLocales->numeroJuegos + cantidadNuevaJuegos; i++) {
-        printf("\n\tDame el año de salida del juego del personaje %d: ", i + 1);
+        printf("\n\tDame el aÃ±o de salida del juego del personaje %d: ", i + 1);
         scanf("%d", &(juegosLocales->juegos[i].anoJuego));
 
-        printf("\n\tDame el género del juego del personaje %d (de 1 a 4) : ", i + 1);
+        printf("\n\tDame el gÃ©nero del juego del personaje %d (de 1 a 4) : ", i + 1);
         scanf("%d", &(juegosLocales->juegos[i].genero));
 
         char cadenaAuxiliar[100];
@@ -50,9 +50,9 @@ void llenarConjuntoPersonajes(conjuntoPersonajes * juegosLocales) //Paso por ref
         strcpy(juegosLocales->juegos[i].nombrePersonaje, cadenaAuxiliar);
     }
 
-    // Actualizo el número total de personajes
+    // Actualizo el nÃºmero total de personajes
     juegosLocales->numeroJuegos += cantidadNuevaJuegos;
-    printf("\n\tPersonajes añadidos con exito, recuerde guardarlos en el fichero");
+    printf("\n\tPersonajes aÃ±adidos con exito, recuerde guardarlos en el fichero");
 }
 
 void llenarConjuntoPersonajesFichero(conjuntoPersonajes * juegosLocales)
@@ -97,8 +97,8 @@ void mostrarPersonajes(conjuntoPersonajes juegosLocales)
     for (i=0; i<juegosLocales.numeroJuegos;i++){
 
         printf("\n\n\tNOMBRE: %s", juegosLocales.juegos[i].nombrePersonaje);
-        printf("\n\tAÑO: %d", juegosLocales.juegos[i].anoJuego);
-        printf("\n\tGÉNERO: ");
+        printf("\n\tAÃ‘O: %d", juegosLocales.juegos[i].anoJuego);
+        printf("\n\tGÃ‰NERO: ");
 
         switch (juegosLocales.juegos[i].genero)
         {
@@ -133,7 +133,7 @@ void eliminarPersonajeEleccion(conjuntoPersonajes * juegosLocales)
         printf("\t%d. %s\n", i + 1, juegosLocales->juegos[i].nombrePersonaje);
     }
 
-    printf("\n\t¿Que personaje desea eliminar?: ");
+    printf("\n\tÂ¿Que personaje desea eliminar?: ");
     scanf("%d", &numeroPersonaje);
 
     if (numeroPersonaje <= 0 || numeroPersonaje > juegosLocales->numeroJuegos) {
@@ -143,7 +143,7 @@ void eliminarPersonajeEleccion(conjuntoPersonajes * juegosLocales)
 
     numeroPersonaje--; //Reducimos el numero de personajes porque lo hemos eliminado
 
-    // Liberar memoria dinámica del nombre del personaje a borrar
+    // Liberar memoria dinÃ¡mica del nombre del personaje a borrar
     free(juegosLocales->juegos[numeroPersonaje].nombrePersonaje);
     juegosLocales->juegos[numeroPersonaje].nombrePersonaje = NULL;
 
@@ -152,10 +152,10 @@ void eliminarPersonajeEleccion(conjuntoPersonajes * juegosLocales)
         juegosLocales->juegos[i] = juegosLocales->juegos[i + 1];
     }
 
-    // Disminuir el número total de personajes
+    // Disminuir el nÃºmero total de personajes
     (juegosLocales->numeroJuegos)--;
 
-    // Reasignar memoria dinámica para el conjunto de personajes
+    // Reasignar memoria dinÃ¡mica para el conjunto de personajes
     juegosLocales->juegos = (Juego *)realloc(juegosLocales->juegos, juegosLocales->numeroJuegos * sizeof(Juego));
 
     printf("\n\tPersonaje eliminado exitosamente. Recuerde guardar los cambios en el fichero\n");
@@ -170,7 +170,7 @@ void eliminarPersonajes( conjuntoPersonajes * juegosLocales)
         return;
     }
 
-    //Elimino la memoria dinámica de cada una de los personajes
+    //Elimino la memoria dinÃ¡mica de cada una de los personajes
     for (i=0; i<juegosLocales->numeroJuegos; i++)
     {
     if (juegosLocales->juegos[i].nombrePersonaje != NULL)
@@ -180,14 +180,14 @@ void eliminarPersonajes( conjuntoPersonajes * juegosLocales)
         }
     }
 
-    //Eliminar la memoria dinñamica del vector que contiene todos los personajes
+    //Eliminar la memoria dinÃ±amica del vector que contiene todos los personajes
     if (juegosLocales->juegos != NULL)
     {
     free(juegosLocales->juegos);
     juegosLocales->juegos = NULL;
     }
 
-    // Actualizo el número de personajes a cero
+    // Actualizo el nÃºmero de personajes a cero
     juegosLocales->numeroJuegos = 0;
 
     printf("\n\n\t\tTODOS LOS PERSONAJES HAN SIDO ELIMINADOS. Recuerde guardar los cambios en el fichero\n");
@@ -214,13 +214,13 @@ void guardarDatosFichero(conjuntoPersonajes * juegosLocales){
 int main(int argc, char *argv[])
 {
     srand(time(NULL));//Semilla para numero aleatorio para incializar tres personajes
-    setlocale(LC_ALL,""); // Configurar la localización para UTF-8 para poner la ñ y acentos
+    setlocale(LC_ALL,""); // Configurar la localizaciÃ³n para UTF-8 para poner la Ã± y acentos
     int eleccion = 0;
 
     conjuntoPersonajes misPersonajes = {NULL , 0}; //Declaro mi estructra de personajes
     llenarConjuntoPersonajesFichero(&misPersonajes); //Le paso mis personajes por referencia;
     do {
-        printf("\n\n\t¿Que desea hacer?\n\tBorrar personaje = 1\n\tAñadir personaje = 2\n\tListar personajes = 3\n\tEliminar todos los personajes = 4\n\tGuardar personajes en fichero = 5\n\tSalir = 6\n\tElección->");
+        printf("\n\n\tÂ¿Que desea hacer?\n\tBorrar personaje = 1\n\tAÃ±adir personaje = 2\n\tListar personajes = 3\n\tEliminar todos los personajes = 4\n\tGuardar personajes en fichero = 5\n\tSalir = 6\n\tElecciÃ³n->");
         scanf("%d",&eleccion);
 
         switch (eleccion){
